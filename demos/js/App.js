f991a469bc13d79985e027a347bd9eacab8a4bcf
Gael Hugo <gael.hugo@onemore-studio.com>
2017-03-08 18:43:37 +0100

    Merge App into romain
    # Conflicts:
    #	demos/index.html
    #	demos/js/App.js

--- demos/js/App.js
+++ demos/js/App.js
@@ -1,4 +1,5 @@
 var App = function() {
+<<<<<<< HEAD
     console.log("app is running");
     this.canvas = document.getElementsByTagName("canvas")[0];
     this.w = window.innerWidth;
@@ -10,10 +11,25 @@ var App = function() {
     this.isMic = false;
     this.setup();
 
+=======
+  console.log("app is running");
+  this.canvas = document.getElementsByTagName("canvas")[0];
+  // document.body.style.background = "black";
+  this.w = window.innerWidth;
+  this.h = window.innerHeight;
+  this.canvas.width = this.w;
+  this.canvas.height = this.h;
+  this.ctx = this.canvas.getContext("2d");
+  this.tool = null;
+  this.isMic = false;
+  this.setup();
+  this.start();
+>>>>>>> master
 };
 
 App.prototype = {
 
+<<<<<<< HEAD
     setup : function() {
         document.addEventListener("keydown", this.onKeyDown.bind(this));
 
@@ -36,6 +52,27 @@ App.prototype = {
     },
 
     // start : function() { this.draw(); },
+=======
+  setup : function() {
+    // this.SPACING = 2;
+    // this.BAR_WIDTH = 1;
+    // this.numBars = Math.round(this.w / this.SPACING);
+    // this.multiplier = 4;
+    addEventListener("keydown", this.onKeyDown.bind(this));
+
+    // DEMOS
+    // this.barDemo = new BarsDemo(this.ctx, this.w, this.h);
+    // this.circleDemo = new CircleBars(this.ctx, this.w, this.h);
+    // this.circleDemo.rotation = true;
+    // this.letterDemo = new LetterDemo(this.canvas, this.w, this.h);
+    // this.rasterDemo = new RasterDemo(this.canvas, this.w, this.h);
+    // this.beatDemo = new BeatDemo(this.ctx, this.w, this.h);
+    // this.verletDemo = new VerletDemo(this.canvas, this.w, this.h);
+    this.joyDivision = new JoyDivision(this.ctx, this.w, this.h);
+  },
+
+  start : function() { this.draw(); },
+>>>>>>> master
 
     draw : function() {
         // clean canvas
@@ -46,6 +83,11 @@ App.prototype = {
             this.tool.updateWave();
             // this.barDemo.draw(this.tool.data);
             // this.circleDemo.draw(this.tool.data);
+<<<<<<< Updated upstream
+=======
+
+<<<<<<< HEAD
+>>>>>>> Stashed changes
             // this.letterDemo.draw(this.tool.dataWave);
             // this.rasterDemo.draw(this.tool.data);
             // this.beatDemo.draw(this.tool.data, this.tool.dataWave);
@@ -81,6 +123,34 @@ App.prototype = {
                 // }
             }
             break;
+=======
+      // this.letterDemo.draw(this.tool.dataWave);
+      // this.rasterDemo.draw(this.tool.data);
+      // this.beatDemo.draw(this.tool.data, this.tool.dataWave);
+      // this.verletDemo.draw(this.tool.data);
+      this.joyDivision.draw(this.tool.data);
+    }
+    // refresh
+    requestAnimationFrame(this.draw.bind(this));
+  },
+
+  onKeyDown : function(e) {
+    var track = "audio/click.mp3";
+    switch (e.keyCode) {
+    case 32: // spacebar
+      if (this.tool == null) {
+        this.tool = new AudioTool(track);
+        this.tool.toggle();
+      } else {
+        this.tool.reset();
+        if (this.isMic) {
+          this.tool.update(track);
+          this.tool.toggle();
+          this.isMic = false;
+        } else {
+          this.tool.update(null);
+          this.isMic = true;
+>>>>>>> master
         }
     }
 
