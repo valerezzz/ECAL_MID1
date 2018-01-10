3cca24cab6effd1f4fefb2d8f38609858faa4e4d
Gael Hugo <gael.hugo@onemore-studio.com>
2018-01-10 18:06:44 +0100

    Remove comments

--- 2018/trail/index.js
+++ 2018/trail/index.js
@@ -18,19 +18,6 @@ let mouseX, mouseY, width, height;
 width = window.innerWidth;
 height = window.innerHeight;
 
-let my_gradient = ctx.createLinearGradient(0, 0, 0, 170);
-my_gradient.addColorStop(0, 'black');
-my_gradient.addColorStop(1, 'white');
-
-
-// let letters = [
-//  new Letter('R', 300, -Math.PI, 0.3),   //
-//  new Letter('A', 150, -Math.PI, -0.6),  //
-//  new Letter('D', 0, 0, 0.1),           //
-//  new Letter('A', 150, 0, -0.6),         //
-//  new Letter('R', 300, 0, 0.3)
-//];
-
 function initialise() {
   var canvas = document.getElementById('canvas');
   canvas.addEventListener('mousedown', doMouseDown, false);
@@ -42,18 +29,6 @@ function initialise() {
 function draw() {
   ctx.fillStyle = 'rgba(0,0,0,0.1)';
   ctx.fillRect(0, 0, width, height);
-
-  // let x = e.offsetX;
-  // let y = e.offsetY;
-
-  // let pos = getMousePos(canvas, e);
-  // let posx = pos.x;
-  // let posy = pos.y;
-
-
-  // my_gradient.addColorStop(0, 'black');
-  // my_gradient.addColorStop(1, 'white');
-  // ctx.fillStyle = my_gradient;
   ctx.fillStyle = 'white';
   ctx.fillRect(mouseX - 1, canvas.height / 2 - 85, 3, 200);
   ctx.fillRect(canvas.width - mouseX, canvas.height / 2 - 85, 3, 200);
@@ -68,34 +43,6 @@ function onMouseMove(e) {
   mouseY = e.pageY;
 }
 
-// function getMousePos(canvas, evt) {
-//   var rect = canvas.getBoundingClientRect();
-//   return {x: evt.clientX - rect.left, y: evt.clientY - rect.top};
-// }
-
-
-
-// function draw() {
-//
-//
-//  ctx.fillStyle = "white";
-//  ctx.fill();
-//  ctx.rect(mousePos.x, 10, 3, 60);
-//  //ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-//  w.draw();
-//  drawText();
-//
-//
-//
-//}
-//
-// function getMousePos(canvas, evt) {
-//    var rect = canvas.getBoundingClientRect();
-//    return {
-//      x: evt.clientX - rect.left,
-//      y: evt.clientY - rect.top
-//    };
-//}
 
 function drawText() {
   ctx.font = '120px replica';
