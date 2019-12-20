dea4e3d786d02ad6a36191249caf76f70701c9af
Gael Hugo <gael.hugo@onemore-studio.com>
2019-12-20 08:14:35 +0100

    Update to p5js

--- 2020/Html_grid/js/App.js
+++ 2020/Html_grid/js/App.js
@@ -2,10 +2,14 @@ class App {
   constructor() {
     this.grid = new Grid();
     //
-    const layer1 = this.grid.cases[0];
     // lettre A could be any class with a canvas
     // we just need a container (layer1)
+    const layer1 = this.grid.cases[0];
     this.lettreA = new LettreA(layer1);
+    const layer2 = this.grid.cases[1];
+    this.letterP5 = new LettreP5(layer2);
+    const layer3 = this.grid.cases[2];
+    this.letterPaper = new LettrePaper(layer3);
     this.setup();
   }
   setup() {
@@ -15,10 +19,14 @@ class App {
   draw() {
     this.lettreA.update();
     this.lettreA.draw();
+    this.letterP5.update();
+    this.letterP5.draw();
     requestAnimationFrame(this.draw.bind(this));
   }
 }
 
 window.onload = function() {
   new App();
-}
+};
+// force p5
+function setup() {}
---
+++ 2020/Html_grid/js/LettreP5.js
@@ -0,0 +1,73 @@
+class LettreP5 {
+  constructor(parent) {
+    this.parent = parent;
+    this.w = parseFloat(parent.style.width);
+    this.h = parseFloat(parent.style.height);
+    // p5 canvas
+    this.canvas = createCanvas(this.w, this.h);
+    this.canvas.canvas.style.position = 'absolute';
+    this.canvas.parent(parent);
+    // init p5 VARS...
+    fill(255, 0, 0, 126);
+    this.gravity = 9.0;
+    this.mass = 2.0;
+    this.s1 = new Spring2D(0.0, width / 2, this.mass, this.gravity);
+    this.s2 = new Spring2D(0.0, width / 2, this.mass, this.gravity);
+  }
+  update() {
+    this.w = parseInt(this.parent.style.width);
+    this.h = parseInt(this.parent.style.height);
+    // p5 resize canvas
+    resizeCanvas(this.w, this.h);
+
+    if (this.w >= window.innerWidth && this.h >= window.innerHeight) {
+      this.active = true;
+    } else {
+      this.active = false;
+    }
+  }
+
+  draw() {
+    if (this.active) {
+      background(0);
+    }
+    this.s1.update(mouseX, mouseY);
+    this.s1.display(mouseX, mouseY);
+    this.s2.update(this.s1.x, this.s1.y);
+    this.s2.display(this.s1.x, this.s1.y);
+  }
+}
+
+
+class Spring2D {
+  constructor(xpos, ypos, m, g) {
+    this.x = xpos;  // The x- and y-coordinates
+    this.y = ypos;
+    this.vx = 0;  // The x- and y-axis velocities
+    this.vy = 0;
+    this.mass = m;
+    this.gravity = g;
+    this.radius = 30;
+    this.stiffness = 0.2;
+    this.damping = 0.7;
+  }
+
+  update(targetX, targetY) {
+    let forceX = (targetX - this.x) * this.stiffness;
+    let ax = forceX / this.mass;
+    this.vx = this.damping * (this.vx + ax);
+    this.x += this.vx;
+    let forceY = (targetY - this.y) * this.stiffness;
+    forceY += this.gravity;
+    let ay = forceY / this.mass;
+    this.vy = this.damping * (this.vy + ay);
+    this.y += this.vy;
+  }
+
+  display(nx, ny) {
+    noStroke();
+    ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+    stroke(255);
+    line(this.x, this.y, nx, ny);
+  }
+}
